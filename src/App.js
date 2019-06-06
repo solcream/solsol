7459e3efd7dd193ba7164262f3680c8f80aa430e
solcream <47736779+solcream@users.noreply.github.com>
2019-06-06 12:05:37 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import WithFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
+// jag tar bort import av WithFirebaseAuth //
 import "./App.css";
 import Nyhetslista from './component/Nyhetslista'
-
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 class App extends Component {
   /* Ställ in komponentens startläge
 Peka på det globala sammanhanget för det här sökordet.*/
   constructor(props) {
     super(props);
     this.state = {
-      articles: []};
-      }
+      articles: [
+      {
+
+    }]};
+  }
 
   componentDidMount() {
     /* lägger in koden för nyhetsartiklar */
@@ -42,49 +38,24 @@ Peka på det globala sammanhanget för det här sökordet.*/
   }
 
   render() {
-    const {
-      user,
-      signOut,
-    SignInWithGoogle,
-    } = this.props;
     return (
+
+
       <div className="App">
         <header className="App-header">
-          
-    <div>
-        {
-          // Knapp för att logga in och ut med Google//         
-            user
-            ? <p>Hello, {user.displayName}</p>
-            : <p>Please sign in.</p>
-        }
- 
-        {
-          user
-            ? <button onClick={signOut}>Sign out</button>
-            : <button onClick={SignInWithGoogle}>Sign in with Google</button>
-        }
-
-        </div>
-
-      <h1>Dagens nyheter</h1>
+   <h1>Dagens nyheter</h1>
       <p className="underrubrik">Här kan du läsa dagens nyheter</p>
+
 </header>
 
       <Nyhetslista
         /* rubrikomponent */
         minaArtiklar={this.state.articles} />
-    </div>
+
+        </div>
+    
     );
       }
     }
-   const firebaseAppAuth = firebaseApp.auth();
-// se signaturen ovan för att ta reda på den tillgängliga levrantören //
-  const providers = {
-    googleProvider: new firebase.auth.GoogleAuthProvider(),
-  };
-  // slå in det //
-  export default WithFirebaseAuth({
-    providers,
-    firebaseAppAuth,
-   })(App); 
+ 
+  export default App; 
